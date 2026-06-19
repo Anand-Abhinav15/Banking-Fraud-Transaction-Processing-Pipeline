3771f6aecfd2f4c25c0f16c1e6d8876af40f7c06
Abhinav Anand <a8hinav.anand1508@gmail.com>
2026-06-20 00:34:35 +0530

    updated validate.py

--- etl/validate.py
+++ etl/validate.py
@@ -53,5 +53,97 @@ def validate_data(df):
                 "Invalid amount"
             )
 
-            
+        ## Amount Validation
 
+        try:
+
+            amount = float(row["amount"])
+
+            if amount <= 0:
+
+                errors.append(
+                    "Negative amount"
+                ) 
+
+        except:
+
+            errors.append(
+                "Invalid amount"
+            )
+
+        
+        ## Currency Validation
+
+        if row["currency"] not in allowed_currencies:
+
+            errors.append(
+                "Invalid currency"
+            )
+            
+        ## Timestamp validation
+
+        try:
+
+            pd.to_datetime(
+                row["transaction_timestamp"]
+            )
+
+        except:
+            
+            errors.append(
+                "Invalid timestamp"
+            )
+
+        ## Account Validation
+
+        account = str(
+            row["account_number"]
+        )
+
+        if not (
+            account.startswith("ACC")
+            and len(account) == 11
+        ):
+            
+            errors.append(
+                "Invalid account number"
+            )
+
+        ## Classification
+
+        if errors:
+
+            row_copy = row.copy()
+
+            row_copy["rejection_reason"] = (
+                " | ".join(errors)
+            )
+
+            rejected_records.append(
+                row_copy
+            )
+
+        else:
+
+            valid_records.append(row)
+
+    valid_df = pd.DataFrame(valid_records)
+
+    rejected_records = pd.DataFrame(
+        rejected_records
+    )
+
+    logger.info(
+        f"Valid records: {len(valid_df)}"
+    )
+
+    logger.info(
+        f"Rejected records: {len(rejected_records)}"
+    )
+
+    rejected_records.to_csv(
+        "data/rejected/rejected_transactions.csv",
+        index = False
+    )
+
+    return valid_df
